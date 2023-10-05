7fd75b033e5a08b92b88bdf28a7f4f493027319b
ibrahim <solaadams423@gmail.com>
2023-10-05 10:25:32 +0100

    commit

---
+++ pages/login.js
@@ -0,0 +1,180 @@
+import Head from "next/head";
+import React from "react";
+import { BsPersonCircle } from "react-icons/bs";
+import { HiMail } from "react-icons/hi";
+import { RiLockPasswordFill } from "react-icons/ri";
+import "../app/globals.css";
+import Link from "next/link";
+
+const Login = () => {
+  return (
+    <React.Fragment>
+      <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 p-3">
+        <div
+          className="
+          flex flex-col
+          bg-white
+          shadow-md
+          px-4
+          sm:px-6
+          md:px-8
+          lg:px-10
+          py-8
+          rounded-3xl
+          max-w-md w-full
+        "
+        >
+          <div className="font-medium self-center text-xl sm:text-3xl text-gray-800">
+            Welcome back
+          </div>
+          <div className="mt-4 self-center text-xl sm:text-sm text-gray-800">
+            Enter your credentials to get access account
+          </div>
+
+          <div className="mt-10">
+            <form action="#">
+              <div className="flex flex-col mb-5"></div>
+              <div className="flex flex-col mb-5">
+                <label
+                  htmlFor="email"
+                  className="mb-1 text-xs tracking-wide text-gray-600"
+                >
+                  E-Mail Address:
+                </label>
+                <div className="relative">
+                  <div
+                    className="
+                    inline-flex
+                    items-center
+                    justify-center
+                    absolute
+                    left-0
+                    top-0
+                    h-full
+                    w-10
+                    text-gray-400
+                  "
+                  >
+                    <HiMail className="text-blue-500" />
+                  </div>
+
+                  <input
+                    id="email"
+                    type="email"
+                    name="email"
+                    className="
+                    text-sm
+                    placeholder-gray-500
+                    pl-10
+                    pr-4
+                    rounded-2xl
+                    border border-gray-400
+                    w-full
+                    py-2
+                    focus:outline-none focus:border-blue-400
+                  "
+                    placeholder="Enter your email"
+                  />
+                </div>
+              </div>
+              <div className="flex flex-col mb-6">
+                <label
+                  htmlFor="password"
+                  className="mb-1 text-xs sm:text-sm tracking-wide text-gray-600"
+                >
+                  Password:
+                </label>
+                <div className="relative">
+                  <div
+                    className="
+                    inline-flex
+                    items-center
+                    justify-center
+                    absolute
+                    left-0
+                    top-0
+                    h-full
+                    w-10
+                    text-gray-400
+                  "
+                  >
+                    <span>
+                      <RiLockPasswordFill className="text-blue-500" />
+                    </span>
+                  </div>
+
+                  <input
+                    id="password"
+                    type="password"
+                    name="password"
+                    className="
+                    text-sm
+                    placeholder-gray-500
+                    pl-10
+                    pr-4
+                    rounded-2xl
+                    border border-gray-400
+                    w-full
+                    py-2
+                    focus:outline-none focus:border-blue-400
+                  "
+                    placeholder="Enter your password"
+                  />
+                </div>
+              </div>
+
+              <div className="flex w-full">
+                <button
+                  type="submit"
+                  className="
+                  flex
+                  mt-2
+                  items-center
+                  justify-center
+                  focus:outline-none
+                  text-white text-sm
+                  sm:text-base
+                  bg-blue-500
+                  hover:bg-blue-600
+                  rounded-2xl
+                  py-2
+                  w-full
+                  transition
+                  duration-150
+                  ease-in
+                "
+                >
+                  <span className="mr-2 uppercase">Sign Up</span>
+                  <span>
+                    <svg
+                      className="h-6 w-6"
+                      fill="none"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
+                  </span>
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div className="flex justify-center items-center mt-6">
+          <span className="ml-2 text-black">New member?</span>
+          <Link
+            href="/signup"
+            className="text-xs ml-2 text-blue-500 font-semibold"
+          >
+            Sign up
+          </Link>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Login;
